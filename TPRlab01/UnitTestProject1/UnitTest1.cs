9453003c4e543aaa8a97d45b9d9053d7f9ad1b75
Daniel Łondka <224360@edu.p.lodz.pl>
2020-11-02 00:15:15 +0100

    Initial commit, simple method and test

---
+++ TPRlab01/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        Class1 class1 = new Class1();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(class1.add(2, 0.231), 2 + 0.231);
+        }
+    }
+}
