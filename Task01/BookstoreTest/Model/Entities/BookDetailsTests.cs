ad0309d9e48e702b82167a4752815724e22a20ef
Daniel Łondka <224360@edu.p.lodz.pl>
2020-11-14 21:54:34 +0100

    mid writing BookDetails test

--- Task01/BookstoreTest/Model/Entities/BookDetailsTests.cs
+++ Task01/BookstoreTest/Model/Entities/BookDetailsTests.cs
@@ -11,25 +11,33 @@ namespace BookstoreLibrary.Model.Entities.Tests
     [TestClass()]
     public class BookDetailsTests
     {
-        
+        static Book book = new Book("Harry Potter", "J.K. Rowling", 1997);
+        BookDetails bookDetails = new BookDetails(book, new decimal(19.99), new decimal(0.05), 12, "Book about adventures of young Wizzard Harry");
         [TestMethod()]
         public void BookDetailsTest()
         {
-            Book book = new Book("Harry Potter", "J.K. Rowling", 1997);
-            BookDetails bookDetails = new BookDetails(book, new decimal(19.99), new decimal(0.05), 12, "Book about adventures of young Wizzard Harry");
-
+            Assert.IsTrue(bookDetails.Book.Equals(book));
+            Assert.IsTrue(bookDetails.GrossPrice.Equals(new decimal(19.99)));
+            Assert.IsTrue(bookDetails.Tax.Equals(new decimal(0.05)));
+            Assert.IsTrue(bookDetails.Count.Equals(12));
+            Assert.IsTrue(bookDetails.Description.Equals("Book about adventures of young Wizzard Harry"));
         }
 
+
+        BookDetails bd2 = new BookDetails(book, new decimal(20.20), new decimal(0.20), 123, "Cooking book");
         [TestMethod()]
         public void EqualsTest()
         {
-            Assert.Fail();
+            
+            Assert.IsTrue(bookDetails.Equals(bookDetails));
+            Assert.IsFalse(bookDetails.Equals(bd2));
         }
 
         [TestMethod()]
         public void GetHashCodeTest()
         {
-            Assert.Fail();
+            Assert.AreEqual(bookDetails.GetHashCode(), bookDetails.GetHashCode());
+            Assert.AreNotEqual(bookDetails.GetHashCode(), bd2.GetHashCode());
         }
 
         [TestMethod()]
