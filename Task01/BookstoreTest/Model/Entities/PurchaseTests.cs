6fdf98416f8fffa2237856eb6b430ff64114e28b
Daniel Łondka <224360@edu.p.lodz.pl>
2020-11-14 21:12:28 +0100

    add empty classe Stock, Purchase and their tests classes

---
+++ Task01/BookstoreTest/Model/Entities/PurchaseTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookstoreLibrary.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreLibrary.Model.Entities.Tests
+{
+    [TestClass()]
+    public class PurchaseTests
+    {
+        [TestMethod()]
+        public void PurchaseTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Task01/BookstoreTest/Model/Entities/StockTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookstoreLibrary.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreLibrary.Model.Entities.Tests
+{
+    [TestClass()]
+    public class StockTests
+    {
+        [TestMethod()]
+        public void StockTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
