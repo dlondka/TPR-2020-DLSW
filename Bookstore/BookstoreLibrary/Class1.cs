2a94134ff89bf876b1dd8cfe60b82cf183fe8a4e
Szymon Wróbel <szym.wrobel@gmail.com>
2020-11-14 17:03:17 +0100

    code clean up

---
+++ Bookstore/BookstoreLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookstoreLibrary {
+	public class Class1 {
+
+		public Class1() { }
+
+		public double Add(double a, double b) {
+			return a + b;
+		}
+	}
+}
---
+++ Bookstore/BookstoreTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookstoreLibrary;
+
+namespace BookstoreLibrary.Tests {
+
+	[TestClass()]
+	public class UnitTest1 {
+
+		Class1 class1 = new Class1();
+		[TestMethod()]
+		public void AddTest() {
+			Assert.AreEqual(class1.Add(2, 0.231), 2 + 0.231);
+		}
+	}
+}
