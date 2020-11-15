f842cc3e6d8830b9344f2650c5ad4f44812798ae
Szymon Wróbel <szym.wrobel@gmail.com>
2020-11-15 17:04:07 +0100

    Added tests for DataService class

---
+++ Task01/BookstoreTest/Logic/DataServiceTests.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BookstoreLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookstoreLibrary.Tests
+{
+	[TestClass()]
+	public class DataServiceTests
+	{
+		[TestMethod()]
+		public void DataServiceTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void AddBookTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void AddBookDetailsTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void AddClientTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void BuyBookTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void DeleteBookTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void DeleteBookTest1()
+		{
+
+		}
+
+		[TestMethod()]
+		public void DeleteBookDetailsTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void DeleteClientTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void DeletePurchaseTest()
+		{
+			// methods are implemented alphabetically until this one, incusive
+		}
+
+		[TestMethod()]
+		public void GetAllBookDetailsTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void GetAllBooksTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void GetAllClientsTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void GetAllPurchasesTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void GetNumberOfBooksTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void GetNumberOfBooksTest1()
+		{
+
+		}
+
+		[TestMethod()]
+		public void GetPurchasesBetweenTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void GetPurchasesForBookTest()
+		{
+
+		}
+
+		[TestMethod()]
+		public void GetPurchasesForClientTest()
+		{
+
+		}
+	}
+}
