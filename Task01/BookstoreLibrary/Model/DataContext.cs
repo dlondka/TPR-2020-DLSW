38031fce3f817dae46044452fa0421bf3a882a68
Szymon Wróbel <szym.wrobel@gmail.com>
2020-11-27 01:48:06 +0100

    Added Publisher field to DataContext

--- Task01/BookstoreLibrary/Model/DataContext.cs
+++ Task01/BookstoreLibrary/Model/DataContext.cs
@@ -6,6 +6,7 @@ namespace BookstoreLibrary
     public class DataContext
     {
         public List<Client> Clients = new List<Client>();
+        public List<Publisher> Publishers = new List<Publisher>();
         public Dictionary<int, Book> Books = new Dictionary<int, Book>();
         public ObservableCollection<Purchase> Purchases = new ObservableCollection<Purchase>();
         public ObservableCollection<BookDetails> BooksDetails = new ObservableCollection<BookDetails>();
