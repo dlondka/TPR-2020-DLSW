08c1176f89eb54fc040ce0e71a1b36a9c851854b
Szymon Wróbel <szym.wrobel@gmail.com>
2020-11-27 01:45:02 +0100

    Publisher class added. Purchase class changed to abstract. Created SellBook and BuyBook classes, both derive from Purchase class.

---
+++ Task01/BookstoreLibrary/Model/Entities/Publisher.cs
@@ -0,0 +1,14 @@
+﻿namespace BookstoreLibrary
+{
+	public class Publisher
+	{
+		public string PublishersName { get; set; }
+		public string PhoneNumber { get; set; }
+
+		public Publisher(string publishersName, string phoneNumber)
+		{
+			PublishersName = publishersName;
+			PhoneNumber = phoneNumber;
+		}
+	}
+}
--- Task01/BookstoreLibrary/Model/Entities/Purchase.cs
+++ Task01/BookstoreLibrary/Model/Entities/Purchase.cs
@@ -1,41 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace BookstoreLibrary
 {
-	public class Purchase
+	public abstract class Purchase
 	{
-		public Client Client { get; set; }
 		public DateTime PurchaseTime { get; set; }
 		public BookDetails BookDetails { get; set; }
+		public int NumberOfBooks { get; set; }
 
-		public Purchase(Client client, DateTime purchaseTime, BookDetails bookDetails)
+		public Purchase(DateTime purchaseTime, BookDetails bookDetails, int numberOfBooks)
 		{
-			Client = client;
 			PurchaseTime = purchaseTime;
 			BookDetails = bookDetails;
-		}
-
-		public override bool Equals(object obj)
-		{
-			return obj is Purchase purchase &&
-				   EqualityComparer<Client>.Default.Equals(Client, purchase.Client) &&
-				   PurchaseTime == purchase.PurchaseTime &&
-				   EqualityComparer<BookDetails>.Default.Equals(BookDetails, purchase.BookDetails);
-		}
-
-		public override int GetHashCode()
-		{
-			int hashCode = -1512294898;
-			hashCode = hashCode * -1521134295 + EqualityComparer<Client>.Default.GetHashCode(Client);
-			hashCode = hashCode * -1521134295 + PurchaseTime.GetHashCode();
-			hashCode = hashCode * -1521134295 + EqualityComparer<BookDetails>.Default.GetHashCode(BookDetails);
-			return hashCode;
-		}
-
-		public override string ToString()
-		{
-			return $"Purchase:\n{PurchaseTime}\n{Client}{BookDetails}-----";
+			NumberOfBooks = numberOfBooks;
 		}
 	}
 }
