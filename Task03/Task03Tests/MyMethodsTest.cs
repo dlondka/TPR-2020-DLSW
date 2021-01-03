9d2cc8695c3cf44dbbfeaa92dafd600f9a4dd824
Szymon Wróbel <szym.wrobel@gmail.com>
2021-01-03 23:43:26 +0100

    added my tests template

---
+++ Task03/Task03Tests/MyMethodsTest.cs
@@ -0,0 +1,29 @@
+﻿using Task03;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Task03.Tests
+{
+	[TestClass()]
+	public class MyMethodsTest
+	{
+		[TestMethod()]
+		public void M1()
+		{
+			
+		}
+
+		[TestMethod()]
+		public void M2()
+		{
+			
+		}
+
+		[TestMethod()]
+		public void M3()
+		{
+			
+		}
+	}
+}
