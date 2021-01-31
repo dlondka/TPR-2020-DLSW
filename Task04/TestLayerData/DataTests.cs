df8831c5d98ab4e11b26e0c930ae17cd77bcf4ca
Daniel Łondka <224360@edu.p.lodz.pl>
2021-01-31 13:57:21 +0100

    Refactor TestDataLayer

---
+++ Task04/TestLayerData/DataTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using LayerData;
+using System.Linq;
+
+namespace LayerDataTests
+{
+	[TestClass]
+	public class LayerDataTests
+	{
+		[TestMethod]
+		public void AddToAndGetFromDBTest()
+		{
+			using (ContactTypeDBDataContext Context = new ContactTypeDBDataContext())
+			{
+				ContactType newContact = new ContactType
+				{
+					Name = "New contact name",
+					ModifiedDate = DateTime.Now
+				};
+
+				Context.ContactType.InsertOnSubmit(newContact);
+				Context.SubmitChanges();
+			}
+
+			using (ContactTypeDBDataContext Context = new ContactTypeDBDataContext())
+			{
+				ContactType readContact = Context.ContactType.FirstOrDefault(c => c.Name == "New contact name");
+				ContactType anotherReadContact = Context.ContactType.FirstOrDefault(c => c.Name == "A name that does not appear");
+
+				Assert.AreEqual("New contact name", readContact.Name);
+				Assert.IsNull(anotherReadContact);
+
+				Context.ContactType.DeleteOnSubmit(readContact);
+				Context.SubmitChanges();
+
+				ContactType yetAnotherReadContact = Context.ContactType.FirstOrDefault(c => c.Name == "New contact name");
+
+				Assert.IsNull(yetAnotherReadContact);
+			}
+
+
+		}
+	}
+}
