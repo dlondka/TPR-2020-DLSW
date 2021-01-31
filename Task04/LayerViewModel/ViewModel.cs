81304f2f4fccc5e8869e377e25ddc6ba96f25eb1
Szymon Wróbel <szym.wrobel@gmail.com>
2021-01-31 23:04:22 +0100

    Add tests for view model + fixed a few problems

--- Task04/LayerViewModel/ViewModel.cs
+++ Task04/LayerViewModel/ViewModel.cs
@@ -6,180 +6,184 @@ using LayerModel;
 
 namespace LayerViewModel
 {
-    public class ViewModel : ViewModelListener
-    {
-        private List<ContactType> _contactTypes;
-        private ContactType _currentContactType;
-        private int _currentContactTypeID;
-        private string _currentName;
-        private DateTime _currentModifiedDate;
+	public class ViewModel : ViewModelListener
+	{
+		private List<ContactType> _contactTypes;
+		private ContactType _currentContactType;
+		private int _currentContactTypeID;
+		private string _currentName;
+		private DateTime _currentModifiedDate;
 
-        private IContactsModel ContactsModel;
+		private IContactsModel ContactsModel;
 
-        #region Properties
-        public List<ContactType> ContactTypes
-        {
-            get
-            {
-                return _contactTypes;
-            }
-            set
-            {
-                _contactTypes = value;
-                onPropertyChanged();
-            }
-        }
-        public ContactType CurrentContactType
-        {
-            get
-            {
-                return _currentContactType;
-            }
-            set
-            {
-                _currentContactType = value;
-                onPropertyChanged();
-            }
-        }
-        public int CurrentContactTypeID
-        {
-            get
-            {
-                return _currentContactTypeID;
-            }
-            set
-            {
-                _currentContactTypeID = value;
-                onPropertyChanged();
-            }
-        }
-        public string CurrentName
-        {
-            get
-            {
-                return _currentName;
-            }
-            set
-            {
-                _currentName = value;
-                onPropertyChanged();
-            }
-        }
-        public DateTime CurrentModifiedDate
-        {
-            get
-            {
-                return _currentModifiedDate;
-            }
-            set
-            {
-                _currentModifiedDate = value;
-                onPropertyChanged();
-            }
-        }
-        #endregion
+		#region Properties
+		public List<ContactType> ContactTypes
+		{
+			get
+			{
+				return _contactTypes;
+			}
+			set
+			{
+				_contactTypes = value;
+				onPropertyChanged();
+			}
+		}
+		public ContactType CurrentContactType
+		{
+			get
+			{
+				return _currentContactType;
+			}
+			set
+			{
+				_currentContactType = value;
+				onPropertyChanged();
+			}
+		}
+		public int CurrentContactTypeID
+		{
+			get
+			{
+				return _currentContactTypeID;
+			}
+			set
+			{
+				_currentContactTypeID = value;
+				onPropertyChanged();
+			}
+		}
+		public string CurrentName
+		{
+			get
+			{
+				return _currentName;
+			}
+			set
+			{
+				_currentName = value;
+				onPropertyChanged();
+			}
+		}
+		public DateTime CurrentModifiedDate
+		{
+			get
+			{
+				return _currentModifiedDate;
+			}
+			set
+			{
+				_currentModifiedDate = value;
+				onPropertyChanged();
+			}
+		}
+		#endregion
 
-        #region Commands
-        public Command ShowAddWindowProperty { get; private set; }
+		#region Commands
+		public Command ShowAddWindowProperty { get; private set; }
 
-        public Command ShowDetailsWindowProperty { get; private set; }
+		public Command ShowDetailsWindowProperty { get; private set; }
 
-        public Command DeleteContactTypeProperty { get; set; }
+		public Command DeleteContactTypeProperty { get; set; }
 
-        public Command RefreshProperty { get; set; }
+		public Command RefreshProperty { get; set; }
 
-        public Command ConfirmAddProperty { get; set; }
+		public Command ConfirmAddProperty { get; set; }
 
-        public Command ConfirmEditProperty { get; set; }
-        #endregion
+		public Command ConfirmEditProperty { get; set; }
+		#endregion
 
-        #region Windows
-        public Lazy<IWindow> AddWindow { get; set; }
+		#region Windows
+		public Lazy<IWindow> AddWindow { get; set; }
 
-        public Lazy<IWindow> DetailsWindow { get; set; }
-        #endregion
+		public Lazy<IWindow> DetailsWindow { get; set; }
+		#endregion
 
-        public ViewModel()
-        {
-            ContactsModel = new ContactsModel();
-            init();
-        }
+		public ViewModel()
+		{
+			ContactsModel = new ContactsModel();
+			init();
+		}
 
-        public ViewModel(IContactsModel contactsModel)
-        {
-            ContactsModel = contactsModel;
-            init();
-        }
-
-
-        private void init()
-        {
-            ContactTypes = ContactsModel.GetContactTypes();
-            ShowAddWindowProperty = new Command(ShowAddWindow);
-            ShowDetailsWindowProperty = new Command(ShowDetailsWindow);
-            DeleteContactTypeProperty = new Command(DeleteContactType);
-            RefreshProperty = new Command(Refresh);
-            ConfirmAddProperty = new Command(ConfirmAdd);
-            ConfirmEditProperty = new Command(ConfirmEdit);
-        }
+		public ViewModel(IContactsModel contactsModel)
+		{
+			ContactsModel = contactsModel;
+			init();
+		}
 
 
-        #region Actions
+		private void init()
+		{
+			ContactTypes = ContactsModel.GetContactTypes();
+			ShowAddWindowProperty = new Command(ShowAddWindow);
+			ShowDetailsWindowProperty = new Command(ShowDetailsWindow);
+			DeleteContactTypeProperty = new Command(DeleteContactType);
+			RefreshProperty = new Command(Refresh);
+			ConfirmAddProperty = new Command(ConfirmAdd);
+			ConfirmEditProperty = new Command(ConfirmEdit);
+		}
 
-        private void DeleteContactType()
-        {
-            Task.Run(() =>
-            {
-                ContactsModel.RemoveContactType(CurrentContactType.ContactTypeID);
-                Refresh();
-            });
-        }
 
-        private void ConfirmEdit()
-        {
-            Task.Run(() =>
-            {
-                ContactsModel.UpdateContactType(CurrentContactTypeID, CurrentName);
-                Refresh();
-            });
-        }
+		#region Actions
 
-        private void ConfirmAdd()
-        {
-            Task.Run(() =>
-            {
-                ContactsModel.AddContactType(CurrentName);
-                Refresh();
-            });
-        }
+		public void DeleteContactType()
+		{
+			if (CurrentContactType != null)
+			{
+				Task.Run(() =>
+				{
 
-        private void Refresh()
-        {
-            ContactTypes = ContactsModel.GetContactTypes();
-        }
+					ContactsModel.RemoveContactType(CurrentContactType.ContactTypeID);
+					Refresh();
+				});
+			}
+		}
 
-        private void ShowDetailsWindow()
-        {
-            if (CurrentContactType == null)
-            {
-                return;
-            }
-            CurrentContactTypeID = CurrentContactType.ContactTypeID;
-            CurrentName = CurrentContactType.Name;
-            CurrentModifiedDate = CurrentContactType.ModifiedDate;
+		public void ConfirmEdit()
+		{
+			Task.Run(() =>
+			{
+				ContactsModel.UpdateContactType(CurrentContactTypeID, CurrentName);
+				Refresh();
+			});
+		}
 
-            IWindow window = DetailsWindow.Value;
-            window.SetViewModel(this);
-            window.Show();
-        }
+		public void ConfirmAdd()
+		{
+			Task.Run(() =>
+			{
+				ContactsModel.AddContactType(CurrentName);
+				Refresh();
+			});
+		}
 
-        private void ShowAddWindow()
-        {
-            IWindow window = AddWindow.Value;
-            window.SetViewModel(this);
-            window.Show();
-        }
-        #endregion
+		public void Refresh()
+		{
+			ContactTypes = ContactsModel.GetContactTypes();
+		}
 
-    }
+		public void ShowDetailsWindow()
+		{
+			if (CurrentContactType == null)
+			{
+				return;
+			}
+			CurrentContactTypeID = CurrentContactType.ContactTypeID;
+			CurrentName = CurrentContactType.Name;
+			CurrentModifiedDate = CurrentContactType.ModifiedDate;
+
+			IWindow window = DetailsWindow.Value;
+			window.SetViewModel(this);
+			window.Show();
+		}
+
+		public void ShowAddWindow()
+		{
+			IWindow window = AddWindow.Value;
+			window.SetViewModel(this);
+			window.Show();
+		}
+		#endregion
+
+	}
 }
---
+++ Task04/TestLayerViewModel/ViewModelTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LayerViewModel;
+using LayerModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LayerViewModel.Tests
+{
+	[TestClass()]
+	public class ViewModelTests
+	{
+		[TestMethod()]
+		public void AddContactTypeTest()
+		{
+			ViewModel vm = new ViewModel(new ContactsModel());
+			int i = vm.ContactTypes.Count;
+			vm.CurrentName = "Test";
+			vm.ConfirmAdd();
+			System.Threading.Thread.Sleep(300);
+			Assert.AreEqual(i + 1, vm.ContactTypes.Count);
+		}
+
+		[TestMethod()]
+		public void DeleteContactTypeTest()
+		{
+			ViewModel vm = new ViewModel(new ContactsModel());
+			int i = vm.ContactTypes.Count;
+			vm.CurrentContactType = vm.ContactTypes[0];
+			vm.DeleteContactType();
+			System.Threading.Thread.Sleep(300);
+			Assert.AreEqual(i - 1, vm.ContactTypes.Count);
+		}
+
+		[TestMethod()]
+		public void ConfirmEditTest()
+		{
+			ViewModel vm = new ViewModel(new ContactsModel());
+			vm.CurrentContactTypeID = 0;
+			string name = vm.ContactTypes[vm.CurrentContactTypeID].Name;
+			vm.CurrentName = "";
+			vm.ConfirmEdit();
+			Assert.AreEqual("Assistant Sales Agent", vm.ContactTypes[vm.CurrentContactTypeID].Name);
+		}
+	}
+}
