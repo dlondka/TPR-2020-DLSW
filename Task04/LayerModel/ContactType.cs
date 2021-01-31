64b6584fb2fc6096400b7b2b3794e41b838c55cd
Daniel Łondka <224360@edu.p.lodz.pl>
2021-01-31 12:22:31 +0100

    Add ContactType to model layer and ContactTypeWrapper to services layer

---
+++ Task04/LayerModel/ContactType.cs
@@ -0,0 +1,57 @@
+﻿using LayerServices;
+using System;
+
+namespace LayerModel
+{
+    class ContactType
+    {
+        private int _contactTypeID;
+        private string _name;
+        private DateTime _modifiedDate;
+
+
+        public int ContactTypeID
+        {
+            get
+            {
+                return _contactTypeID;
+            }
+            set
+            {
+                _contactTypeID = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public DateTime ModifiedDate
+        {
+            get
+            {
+                return _modifiedDate;
+            }
+            set
+            {
+                _modifiedDate = value;
+            }
+        }
+        public ContactType() {}
+        
+        public ContactType(ContactTypeWrapper contactTypeWrapper)
+        {
+            ContactTypeID = contactTypeWrapper.ContactTypeID;
+            Name = contactTypeWrapper.Name;
+            ModifiedDate = contactTypeWrapper.ModifiedDate;
+        }
+    }
+}
---
+++ Task04/LayerServices/ContactTypeWrapper.cs
@@ -0,0 +1,63 @@
+﻿using LayerData;
+using System;
+
+
+namespace LayerServices
+{
+    public class ContactTypeWrapper
+    {
+        private ContactType ContactType;
+
+        public ContactTypeWrapper(ContactType contactType)
+        {
+            ContactType = contactType;
+        }
+
+        public ContactTypeWrapper(int contactTypeID, string name, DateTime modifiedDate)
+        {
+            ContactType contactType = new ContactType
+            {
+                ContactTypeID = contactTypeID,
+                Name = name,
+                ModifiedDate = modifiedDate
+            };
+            ContactType = contactType;
+        }
+
+        public int ContactTypeID
+        {
+            get
+            {
+                return ContactType.ContactTypeID;
+            }
+            set
+            {
+                ContactType.ContactTypeID = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return ContactType.Name;
+            }
+            set
+            {
+                ContactType.Name = value;
+            }
+        }
+
+        public DateTime ModifiedDate
+        {
+            get
+            {
+                return ContactType.ModifiedDate;
+            }
+            set
+            {
+                ContactType.ModifiedDate = value;
+            }
+        }
+    }
+}
